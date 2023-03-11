d2a2bc3f3f97efb03e8a6eda9848161240849385
SergeyS85 <surzhiks.85@mail.ru>
2023-03-12 02:03:05 +0300

    create 5 pages and nav menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
 
-	// $('body').hide()
 
+// Sticky menu
+// let wrapper = document.querySelector(".wrapper")
+// window.onscroll = function() {myFunction()};
+
+// let navbar = document.querySelector(".container_menu");
+// let sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky_menu")
+//   } else {
+//     navbar.classList.remove("sticky_menu");
+//   }
+// }
+
+// Smooth scroll gsap
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content',
+	smooth: 1.5,
+	effects: false
 })
+
+
