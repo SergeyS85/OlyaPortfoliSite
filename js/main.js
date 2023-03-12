9a3cfb3550c73f85e0bd338bf8351e57701efcec
SergeyS85 <surzhiks.85@mail.ru>
2023-03-12 22:23:55 +0300

    mediaquerys

--- js/main.js
+++ js/main.js
@@ -2,25 +2,31 @@
 
 // Sticky menu
 // let wrapper = document.querySelector(".wrapper")
-// window.onscroll = function() {myFunction()};
-
 // let navbar = document.querySelector(".container_menu");
 // let sticky = navbar.offsetTop;
 
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky_menu")
-//   } else {
-//     navbar.classList.remove("sticky_menu");
-//   }
-// }
 
-// Smooth scroll gsap
+
+
+
+// let menu = document.querySelector(".container_menu")
+// var rect = menu.getBoundingClientRect()
+
+// window.addEventListener('scroll',function(e){
+// 	if (rect.top - window.scrollY <= 0) {
+//     menu.classList.add("sticky_menu")
+//   } else {
+//     menu.classList.remove("sticky_menu");
+//   }
+//  console.log(rect.top - window.scrollY)
+// })
+
+//Smooth scroll gsap
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 ScrollSmoother.create({
 	wrapper: '.wrapper',
 	content: '.content',
-	smooth: 1.5,
+	smooth: 1.1,
 	effects: false
 })
 
